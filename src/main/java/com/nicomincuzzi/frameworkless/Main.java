91dea89792ef9328f6e24efc8276ee32f8afdfc1
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2022-02-17 09:41:43 +0100

    [refactor] Handle running of HTTPServer

--- src/main/java/com/nicomincuzzi/frameworkless/Main.java
+++ src/main/java/com/nicomincuzzi/frameworkless/Main.java
@@ -1,19 +1,25 @@
 package com.nicomincuzzi.frameworkless;
 
+import com.nicomincuzzi.frameworkless.controller.MazeRoute;
 import com.nicomincuzzi.frameworkless.maze.ManagerMaze;
 import com.nicomincuzzi.frameworkless.server.HttpServer;
 import com.nicomincuzzi.frameworkless.maze.fsm.MazeFsm;
+import com.nicomincuzzi.frameworkless.server.HttpServer;
 
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            HttpServer server = new HttpServer(9091, "/mazeroute");
+            server.setServlet(MazeRoute.class, "/api/v1/mazeroute");
 
-/*        HttpServer server = new HttpServer(9091, "/mazeroute");
-        server.setServlet(null, "");
-        server.run();*/
+            server.start();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
 
-        MazeFsm<ManagerMaze> mazeFsm = new MazeFsm<>();
+/*        MazeFsm<ManagerMaze> mazeFsm = new MazeFsm<>();
         ManagerMaze mazeRoutePuzzle = new ManagerMaze(mazeFsm);
-        mazeRoutePuzzle.runMazeRetroRoutePuzzle();
+        mazeRoutePuzzle.runMazeRetroRoutePuzzle();*/
     }
 }
